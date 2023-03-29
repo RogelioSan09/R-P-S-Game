e7963b0251d194e4e6df65c2663cb3fbe3171171
RogelioSan09 <rogeliosnchz@gmail.com>
2023-03-29 12:47:59 -0700

    started variables and alert

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var rock = "R";
+var paper = "P";
+var Scissors = "S";
+var userInput;
+var wins = 0;
+var loses = 0;
+var ties = 0;
+var computerInput;
+
+alert("Welcome! Want to play a game?");
+
+if (userInput == true){
+
+}
+else (userInput == false){
+    alert("Maybe next time!");
+}
