8ff47735b98900eb31f03ce46718361a823f1705
Jordan Cardenas <jordancardenas@Jordans-MBP.attlocal.net>
2023-03-29 16:19:19 -0700

    add java and index file

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,64 @@
-var rock = "R";
-var paper = "P";
-var Scissors = "S";
-var userInput;
-var wins = 0;
-var loses = 0;
-var ties = 0;
-var computerInput;
+var wins = 0
+var losses = 0
+var ties = 0
 
-alert("Welcome! Want to play a game?");
+var options = ["r", "p", "s"];
 
-if (userInput == true){
+var playGame = function() {
 
+var userInput = promptUser();
+
+var computerInput = generateComputerInput();
+alertComputerInput(compterInput);
+checkUserComputerInputCondition(userInput, computerInput);
+};
+  
+function promptUser(){
+var userInput = window.prompt ("Enter r, p or s:");
+  if (!userInput) {
+  return;
+} 
+
+if (userInput !== "r" || userInput !== "p" || userInput !== "s"){
+    promptUser();
 }
-else (userInput == false){
-    alert("Maybe next time!");
+return userInput
 }
+
+
+function generateComputerInput(){
+var index = math.floor(math.random() * options.length);
+var computerInput = options[index];
+return computerInput;
+}
+
+function alertComputerInput(randomComputerInput){
+    window.alert("The computer chose" + randomComputerInput);
+}
+
+function checkUserComputerInputCondition(userInput, computerInput){
+    if (userInput === computerInput) {
+        ties++;
+    } else if (
+        (userInput === "r" && computerInput === "s") ||
+        (userInput === "p" && computerInput === "r") ||
+        (userInput === "s" && computerInput === "p")
+    ){
+        wins ++;
+        window.alert("You win!");
+
+    } else {
+        losses++;
+        window.alert("You lost!");
+    }
+
+    window.alert (
+        "stats:\nWins: " + wins + "\nlosses: " + losses + "\nTies" + ties
+    );
+
+    var playAgain = window.confirm("Play again?");
+
+    if (playAgain) {
+        playGame();
+    }
+};
