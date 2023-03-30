bb173f92ab050d1c05286623374920f3f9f1cce0
RogelioSan09 <rogeliosnchz@gmail.com>
2023-03-30 08:43:14 -0700

    Loads in browser, succesfully greets user and invites to play game, prompts user for an input of r, p, or s

--- assets/script.js
+++ assets/script.js
@@ -2,34 +2,39 @@ var wins = 0
 var losses = 0
 var ties = 0
 
+var userInput = " ";
 var options = ["r", "p", "s"];
+
+var initialUserResponse = confirm("Hello!! Would you like to play Rock, Paper, Scissors?");
+
+    if (initialUserResponse){
+        promptUser();
+    }
+    else{
+        alert("Okay, some other time.");
+    }
 
 var playGame = function() {
 
-var userInput = promptUser();
-
-var computerInput = generateComputerInput();
-alertComputerInput(compterInput);
-checkUserComputerInputCondition(userInput, computerInput);
-};
+    var computerInput = generateComputerInput();
+    alertComputerInput(compterInput);
+    checkUserComputerInputCondition(userInput, computerInput);
+}
   
 function promptUser(){
-var userInput = window.prompt ("Enter r, p or s:");
-  if (!userInput) {
-  return;
-} 
-
-if (userInput !== "r" || userInput !== "p" || userInput !== "s"){
-    promptUser();
+    var userInput = window.prompt ("Enter r, p or s:");
+    if (userInput !== "r" || userInput !== "p" || userInput !== "s"){
+        promptUser();
+    }
+    else{
+    return userInput;
+    }
 }
-return userInput
-}
-
 
 function generateComputerInput(){
-var index = math.floor(math.random() * options.length);
-var computerInput = options[index];
-return computerInput;
+    var index = math.floor(math.random() * options.length);
+    var computerInput = options[index];
+    return computerInput;
 }
 
 function alertComputerInput(randomComputerInput){
@@ -37,28 +42,23 @@ function alertComputerInput(randomComputerInput){
 }
 
 function checkUserComputerInputCondition(userInput, computerInput){
-    if (userInput === computerInput) {
+    if (userInput === computerInput){
         ties++;
-    } else if (
-        (userInput === "r" && computerInput === "s") ||
-        (userInput === "p" && computerInput === "r") ||
-        (userInput === "s" && computerInput === "p")
-    ){
+    }
+    else if ((userInput === "r" && computerInput === "s") ||(userInput === "p" && computerInput === "r") ||(userInput === "s" && computerInput === "p")){
         wins ++;
         window.alert("You win!");
-
-    } else {
+    } 
+    else {
         losses++;
         window.alert("You lost!");
     }
 
-    window.alert (
-        "stats:\nWins: " + wins + "\nlosses: " + losses + "\nTies" + ties
-    );
+    window.alert ("stats:\nWins: " + wins + "\nlosses: " + losses + "\nTies" + ties);
 
     var playAgain = window.confirm("Play again?");
 
     if (playAgain) {
         playGame();
     }
-};
+}
